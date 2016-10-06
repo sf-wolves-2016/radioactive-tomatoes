02d7417062038d0c1145219a688a52c835665504
Dave Henton <email": null>
2016-10-06 12:50:59 -0700

    Fix formatting and change login route to sessions/new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,12 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
   helper_method :current_user
 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
   def authorize
-    redirect_to '/login' unless current_user
+    redirect_to '/sessions/new' unless current_user
   end
 end
