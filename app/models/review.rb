d2a86c781b1954b5ecc8e7fc68aee799dab9996b
Sarah Spikes <sdspikes@cs.stanford.edu>
2016-10-06 12:09:33 -0700

    add some bootstrappyness

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,7 +4,15 @@ class Review < ActiveRecord::Base
   belongs_to :reviewable, polymorphic: true
 
   def actor?
-    reviewable_type == Actor
+    reviewable_type == 'Actor'
+  end
+
+  def reviewable_image_url
+    actor? ? reviewable.headshot_url : reviewable.movie_poster_url
+  end
+
+  def reviewable_main_text
+    actor? ? reviewable.name : reviewable.title
   end
 
   def top_comments
