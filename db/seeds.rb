e5b83cb41af14e9f582658971f80b6950820eb59
Sarah Spikes <sdspikes@cs.stanford.edu>
2016-10-06 11:24:13 -0700

    restore the users/reviews/comments to the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,51 +1,3 @@
-# 5.times do
-#   User.create!(username: Faker::StarWars.character,
-#                  email: Faker::Internet.free_email,
-#               password: "password"
-#               )
-# end
-
-# 10.times do
-#   Movie.create!(title: Faker::Superhero.name,
-#             synopsis: Faker::Hipster.sentence,
-#         release_date: Faker::Date.backward(5000)
-#               )
-# end
-
-# 10.times do
-#   Actor.create!(name: Faker::GameOfThrones.character,
-#                 dob: Faker::Date.backward(5000),
-#                 bio: Faker::ChuckNorris.fact
-#               )
-# end
-
-# 10.times do
-#   Role.create!(movie_id: rand(1..10),
-#                actor_id: rand(1..10),
-#          character_name: Faker::Pokemon.name
-#     )
-# end
-
-# 10.times do
-#   Review.create!(title: Faker::Book.title,
-#               content: Faker::Hipster.paragraph,
-#               upvotes: rand(1..9),
-#               downvotes: rand(1..9),
-#               rating: rand(1..5),
-#               reviewer_id: rand(1..5),
-#               reviewable_id: rand(1..10),
-#               reviewable_type: ['Actor', 'Movie'].sample
-#               )
-# end
-
-# 10.times do
-#   Comment.create!(content: Faker::Hacker.say_something_smart,
-#             commenter_id: rand(1..5),
-#             review_id: rand(1..10)
-#                 )
-# end
-
-
 require 'imdb'
 require 'awesome_print'
 require 'httparty'
@@ -66,3 +18,28 @@ query.movies.each do |m|
     end
 end
 
+5.times do
+  User.create!(username: Faker::StarWars.character,
+                 email: Faker::Internet.free_email,
+              password: "password"
+              )
+end
+
+10.times do
+  Review.create!(title: Faker::Book.title,
+              content: Faker::Hipster.paragraph,
+              upvotes: rand(1..9),
+              downvotes: rand(1..9),
+              rating: rand(1..5),
+              reviewer_id: rand(1..5),
+              reviewable_id: rand(1..10),
+              reviewable_type: ['Actor', 'Movie'].sample
+              )
+end
+
+10.times do
+  Comment.create!(content: Faker::Hacker.say_something_smart,
+             commenter_id: rand(1..5),
+                review_id: rand(1..10)
+                )
+end
