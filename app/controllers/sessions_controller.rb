d915bbf002b65dc99328ac78d06169380e13e5b3
Sarah Spikes <sarah.spikes@devbootcamp.com>
2016-10-07 16:42:39 -0700

    quick fix for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController
 
  def create
-    user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
+    user = User.find_by_email(sessions_params[:email])
+    if user && user.authenticate(sessions_params[:password])
       session[:user_id] = user.id
       redirect_to '/'
     else
@@ -14,5 +14,10 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     redirect_to '/login'
+  end
+
+  private
+  def sessions_params
+    params.require('/sessions/new').permit(:email, :password)
   end
 end
